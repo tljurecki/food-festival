75d79a175dc1ab84d2db12c38cd1a58d231fb8f2
Tanya Jurecki <tljurecki@gmail.com>
2022-06-27 13:24:54 -0400

    used file loader and image webpack loader to reduce image size, added multiple entry points to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,52 @@
 const path = require('path');
 const webpack  = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-    entry: './assets/js/script.js', 
+const config = {
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    }, 
     output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    path: path.join(__dirname + "/dist"),
+    filename: "[name].bundle.js"
+},
+module: {
+    rules: [
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false,
+                        name (file) {
+                            return '[path][name].[ext]';
+                        },
+                        publicPath(url) {
+                            return url.replace('../', '/assets/');
+                        }
+                    }
+                },
+                {
+                    loader: 'image-webpack-loader'
+                }
+            ]
+        }
+    ]
 },
 plugins: [
     new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
+        $: 'jquery',
+        jQuery: 'jquery'
     }),
+    new BundleAnalyzerPlugin({
+        analyzerMode: 'static', //the report outputs to an HTML file in the dist folder
+    })
 ],
 mode: 'development'
 };
+
+module.exports = config;
